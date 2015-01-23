eaafe8764b204bea2b0113764429eeab4b2dcffe
hurricup <hurricup@gmail.com>
2015-01-23 20:52:46 +0300

    Initial commit

---
+++ lib/Class/Visibility.pm
@@ -0,0 +1,49 @@
+package Class::Visibility;
+use strict; use warnings FATAL => 'all'; 
+use parent 'Exporter';
+use 5.016;
+use Carp;
+
+our $VERSION = '1.000000';
+
+our @EXPORT;
+
+
+1;
+__END__
+=head1 NAME
+
+Class::Visibility - Perl implementation of class variables visibility limitation.
+
+=head1 SYNOPSIS
+
+  use Class::Visibility;
+
+=head1 DESCRIPTION
+
+
+=head1 BUGS AND IMPROVEMENTS
+
+If you found any bug and/or want to make some improvement, feel free to participate in the project on GitHub: L<https://github.com/hurricup/Class-Visibility>
+
+=head1 LICENSE
+
+This module is published under the terms of the MIT license, which basically means "Do with it whatever you want". For more information, see the LICENSE file that should be enclosed with this distributions. A copy of the license is (at the time of writing) also available at L<http://www.opensource.org/licenses/mit-license.php>.
+
+=head1 SEE ALSO
+
+=over
+
+=item * Main project repository and bugtracker: L<https://github.com/hurricup/Class-Visibility>
+
+=item * See also: L<Class::Property>. 
+
+=back
+
+=head1 AUTHOR
+
+Copyright (C) 2015 by Alexandr Evstigneev (L<hurricup@evstigneev.com|mailto:hurricup@evstigneev.com>)
+
+
+
+=cut
